11cdc2cf4e8dec2087d65e06811c4e9234738a2e
Arky13 <polygot882@gmail.com>
2025-09-12 18:20:26 +0530

    test

---
+++ prep_dataset/agent.py
@@ -0,0 +1,10 @@
+
+''' in this py file , we will pass the PROMPT and store the reponses from the diffferent  LLMs( agents) 
+agent1 -> too much hallucination 
+agent2 -> medium 
+agent3 - strict 
+
+ with correct hallucination score . 
+'''
+
+
